94683e745897986408e157cd576d773d2e37209c
Minkyu Kim <iosif2@iosif.dev>
2025-09-04 13:21:23 +0900

    feat: add POST method support for health check endpoint and enhance logging
    - Added support for the POST method on the health check endpoint.
    - Enhanced logging to include request method, path, headers, and body for better debugging.

--- src/bin/http-server.rs
+++ src/bin/http-server.rs
@@ -1,12 +1,11 @@
 use axum::http::Request;
-use axum::{Json, Router, body::Body, http::StatusCode, routing::get};
+use axum::{Json, Router, body::Body, http::StatusCode, routing::{get, post}};
 use log::info;
 use reverse_tunnel::common::http_conn::parser::request::HttpRequestParser;
 use reverse_tunnel::common::http_conn::types::{HttpRequest, serialize_http_request_to_bytes};
 use serde::Serialize;
 use std::net::SocketAddr;
 
-// API 응답을 위한 구조체
 #[derive(Serialize)]
 struct HealthCheckResponse {
     status: String,
@@ -18,7 +17,7 @@ async fn main() {
     log4rs::init_file("log4rs.yml", Default::default()).unwrap();
     // 헬스 체크 핸들러 함수를 라우터에 연결
     let app = Router::new().route("/", get(health_check_handler));
-
+    let app = app.merge(Router::new().route("/", post(health_check_handler)));
     // 서버 주소 설정
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     info!("Server running on {}", addr);
@@ -40,6 +39,10 @@ async fn health_check_handler(req: Request<Body>) -> (StatusCode, Json<HealthChe
         "Request is_websocket_upgrade: {:?}",
         parser.is_websocket_upgrade()
     );
+    info!("Request method: {:?}", http_req.method);
+    info!("Request path: {:?}", http_req.path);
+    info!("Request headers: {:?}", http_req.headers);
+    info!("Request body: {:?}", http_req.body);
     let response = HealthCheckResponse {
         status: "ok".to_string(),
         message: "Server is healthy".to_string(),
